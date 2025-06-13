0653269483df6c0aa302f7a4a03cb0c412006b19
Kirill Polenov <polenovkirill@gmail.com>
2025-06-14 00:21:10 +0300

    :construction: aluu done

--- machine/alu.go
+++ machine/alu.go
@@ -1,1 +1,63 @@
 package main
+
+import "math"
+
+const (
+	MAX_NUM int = 1<<31 - 1
+	MIN_NUM int = -1 << 31
+)
+
+type ALU struct {
+	registerFile RegisterFile
+	aluSignal    Signal
+	aluResult    int
+	nz           int
+}
+
+func (alu *ALU) init(rf RegisterFile) ALU {
+	return ALU{registerFile: rf, aluResult: 0, nz: 0}
+}
+func (alu *ALU) selectOperation(signal Signal) {
+	alu.aluSignal = signal
+	alu.performOperation(alu.aluSignal)
+}
+
+func (alu *ALU) performOperation(signal Signal) {
+	switch signal {
+	case alu_add:
+		alu.aluResult = alu.registerFile.leftOut + alu.registerFile.rightOut
+	case alu_sub:
+		alu.aluResult = alu.registerFile.leftOut - alu.registerFile.rightOut
+	case alu_mul:
+		alu.aluResult = alu.registerFile.leftOut * alu.registerFile.rightOut
+	case alu_mulh:
+		alu.aluResult = (alu.registerFile.leftOut * alu.registerFile.rightOut) >> 32
+	case alu_div:
+		alu.aluResult = alu.registerFile.leftOut / alu.registerFile.rightOut
+	case alu_and:
+		alu.aluResult = alu.registerFile.leftOut & alu.registerFile.rightOut
+	case alu_or:
+		alu.aluResult = alu.registerFile.leftOut | alu.registerFile.rightOut
+	case alu_xor:
+		alu.aluResult = alu.registerFile.leftOut ^ alu.registerFile.rightOut
+	}
+	alu.handleOverflow()
+}
+
+func (alu *ALU) handleOverflow() {
+	if alu.aluResult > MAX_NUM {
+		alu.aluResult %= MAX_NUM
+	} else if alu.aluResult < MAX_NUM {
+		alu.aluResult %= int(math.Abs(float64(MIN_NUM)))
+	}
+}
+
+func (alu *ALU) setFlags(result int) {
+	if result == 0 {
+		alu.nz = 0b01
+	} else if result < 0 {
+		alu.nz = 0b10
+	} else {
+		alu.nz = 0b00
+	}
+}
